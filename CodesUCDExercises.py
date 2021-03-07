b3860b808d78f57f584222e0f8e12c099bdf6eea
AoifMurphy <79363725+AoifMurphy@users.noreply.github.com>
2021-03-07 16:49:36 +0000

    commit

---
+++ CodesUCDExercises.py
@@ -0,0 +1,169 @@
+print("Hello World")
+
+# Example, do not modify!
+print(5 / 8)
+print(5 / 8)
+print(7 + 10)
+# Print the sum of 7 and 10
+print(7+10)
+
+# Division
+print(5 / 8)
+
+# Addition
+print(7 + 10)
+
+# Addition, subtraction
+print(5 + 5)
+print(5 - 5)
+
+# Multiplication, division, modulo, and exponentiation
+print(3 * 5)
+print(10 / 2)
+print(18 % 7)
+print(4 ** 2)
+
+# How much is your $100 worth after 7 years?
+print(100 * 1.1 ** 7)
+
+# Create a variable savings
+savings = 100
+
+# Print out savings
+print(savings)
+
+# Create a variable savings
+savings = 100
+
+# Create a variable growth_multiplier
+growth_multiplier = 1.1
+
+# Calculate result
+result = 100 * growth_multiplier ** 7
+
+# Print out result
+print(result)
+
+# Create a variable savings
+savings = 100
+
+# Create a variable desc
+desc = "compound interest"
+
+# Create a variable profitable
+profitable = True
+
+savings = 100
+growth_multiplier = 1.1
+desc = "compound interest"
+
+# Assign product of growth_multiplier and savings to year1
+year1 = growth_multiplier * savings
+
+# Print the type of year1
+print(type(year1))
+
+# Assign sum of desc and desc to doubledesc
+doubledesc = desc + desc
+
+# Print out doubledesc
+print(doubledesc)
+
+
+# Definition of savings and result
+savings = 100
+result = 100 * 1.10 ** 7
+
+# Fix the printout
+print("I started with $" + str(savings) + " and now have $" + str(result) + ". Awesome!")
+
+# Definition of pi_string
+pi_string = "3.1415926"
+
+# Convert pi_string into float: pi_float
+pi_float = float(pi_string)
+
+# area variables (in square meters)
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
+
+# Create list areas
+areas = [hall, kit, liv, bed, bath]
+
+# Print areas
+print(areas)
+
+# area variables (in square meters)
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.50
+
+# Adapt list areas
+areas = ["hallway", hall, "kitchen", kit, "living room", liv, "bedroom", bed, "bathroom", bath]
+
+# Print areas
+print(areas)
+
+# house information as list of lists
+house = [["hallway", hall],
+         ["kitchen", kit],
+         ["living room", liv],
+         ["bedroom", bed],
+         ["bathroom", bath]]
+
+# Print out house
+print(house)
+
+# Print out the type of house
+print(type(house))
+
+
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Print out second element from areas
+print(areas[1])
+
+# Print out last element from areas
+print(areas[-1])
+
+# Print out the area of the living room
+print(areas[5])
+
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Sum of kitchen and bedroom area: eat_sleep_area
+eat_sleep_area = areas[3] + areas[-3]
+
+# Print the variable eat_sleep_area
+print(eat_sleep_area)
+
+
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Use slicing to create downstairs
+downstairs = areas [0:6]
+
+# Use slicing to create upstairs
+upstairs = areas [6:10]
+
+# Print out downstairs and upstairs
+print(downstairs)
+print (upstairs)
+
+
+# Create the areas list
+areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
+
+# Alternative slicing to create downstairs
+downstairs = areas[:6]
+
+# Alternative slicing to create upstairs
+upstairs = areas[6:]
