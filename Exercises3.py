e4770af4fc29297bc951656fdd55230a16f76b76
AoifMurphy <79363725+AoifMurphy@users.noreply.github.com>
2021-03-17 19:55:19 +0000

    march 17 updated

--- Exercises3.py
+++ Exercises3.py
@@ -36,74 +36,131 @@ with open('MobyDick.txt') as file:
     print(file.readline())
 
 
-    # Assign filename: file
-    file = 'Seaslug.txt'
 
-    # Import file: data
-    data = np.loadtxt(file, delimiter='\t', dtype=str)
+# Import pandas as pd
+import pandas as pd
 
-    # Print the first element of data
-    print(data[0])
+# Assign the filename: file
+file = 'Titanic.csv'
 
-    # Import data as floats and skip the first row: data_float
-    data_float = np.loadtxt(file, delimiter='\t', dtype='float', skiprows=1)
+# Read the file into a DataFrame: df
+df = pd.read_csv(file)
 
-    # Print the 10th element of data_float
-    print(data_float[9])
+# View the head of the DataFrame
+print(df.head())
 
-    # Plot a scatterplot of the data
-    plt.scatter(data_float[:, 0], data_float[:, 1])
-    plt.xlabel('time (min.)')
-    plt.ylabel('percentage of larvae')
-    plt.show()
 
-    # Assign the filename: file
-    file = 'Titanic.csv'
+# Assign filename: file
+file = 'Seaslug.txt'
 
-    # Import file using np.recfromcsv:
-    d = np.recfromcsv(file)
+# Import file: data
+data = np.loadtxt(file, delimiter='\t', dtype=str)
 
-    # Print out first three entries of d
-    print(d[:3])
+# Print the first element of data
+print(data[0])
 
-    # Assign filename: file
-    file = 'Seaslug.txt'
+# Import data as floats and skip the first row: data_float
+data_float = np.loadtxt(file, delimiter='\t', dtype='float', skiprows=1)
 
-    # Import file: data
-    data = np.loadtxt(file, delimiter='\t', dtype=str)
+# Print the 10th element of data_float
+print(data_float[9])
 
-    # Print the first element of data
-    print(data[0])
+# Assign the filename: file
+file = 'Titanic.csv'
 
-    # Import data as floats and skip the first row: data_float
-    data_float = np.loadtxt(file, delimiter='\t', dtype='float', skiprows=1)
+# Import file using np.recfromcsv:
+d = np.recfromcsv(file)
 
-    # Print the 10th element of data_float
-    print(data_float[9])
+# Print out first three entries of d
+print(d[:3])
 
-    # Plot a scatterplot of the data
-    plt.scatter(data_float[:, 0], data_float[:, 1])
-    plt.xlabel('time (min.)')
-    plt.ylabel('percentage of larvae')
-    plt.show()
 
-    # Assign filename: file
-    file = 'Seaslug.txt'
+# Plot a scatterplot of the data
+plt.scatter(data_float[:, 0], data_float[:, 1])
+plt.xlabel('time (min.)')
+plt.ylabel('percentage of larvae')
+plt.show()
 
-    # Import file: data
-    data = np.loadtxt(file, delimiter='\t', dtype=str)
+# Assign filename: file
+file = 'Seaslug.txt'
 
-    # Print the first element of data
-    print(data[0])
+# Import file: data
+data = np.loadtxt(file, delimiter='\t', dtype=str)
 
-    # Import data as floats and skip the first row: data_float
-    data_float = np.loadtxt(file, delimiter='\t', dtype='float', skiprows=1)
+# Print the first element of data
+print(data[0])
 
-    # Print the 10th element of data_float
-    print(data_float[9])
+# Import data as floats and skip the first row: data_float
+data_float = np.loadtxt(file, delimiter='\t', dtype='float', skiprows=1)
 
-    # Plot a scatterplot of the data
-    plt.scatter(data_float[:, 0], data_float[:, 1])
-    plt.xlabel('time (min.)')
-    plt.ylabel('percentage of larvae')
-    plt.show()
+# Print the 10th element of data_float
+print(data_float[9])
+
+# Plot a scatterplot of the data
+plt.scatter(data_float[:, 0], data_float[:, 1])
+plt.xlabel('time (min.)')
+plt.ylabel('percentage of larvae')
+plt.show()
+
+# Assign filename: file
+ file = 'Seaslug.txt'
+
+# Import file: data
+data = np.loadtxt(file, delimiter='\t', dtype=str)
+
+# Print the first element of data
+print(data[0])
+
+# Import data as floats and skip the first row: data_float
+data_float = np.loadtxt(file, delimiter='\t', dtype='float', skiprows=1)
+
+# Print the 10th element of data_float
+print(data_float[9])
+
+# Plot a scatterplot of the data
+plt.scatter(data_float[:, 0], data_float[:, 1])
+plt.xlabel('time (min.)')
+plt.ylabel('percentage of larvae')
+plt.show()
+
+# Import pandas as pd
+import pandas as pd
+
+# Assign the filename: file
+file = 'Titanic.csv'
+
+# Read the file into a DataFrame: df
+df = pd.read_csv(file)
+
+# View the head of the DataFrame
+print(df.head())
+
+# Assign the filename: file
+file = 'digits.csv'
+
+# Read the first 5 rows of the file into a DataFrame: data
+data = pd.read_csv(file, nrows=5, header=None)
+
+# Build a numpy array from the DataFrame: data_array
+data_array = data.values
+
+# Print the datatype of data_array to the shell
+print(type(data_array))
+
+# Import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
+# Assign filename: file
+file = 'titanic_corrupt.txt'
+
+# Import file: data
+data = pd.read_csv(file, sep='\t', comment='#', na_values='Nothing')
+
+# Print the head of the DataFrame
+print(data.head())
+
+# Plot 'Age' variable in a histogram
+pd.DataFrame.hist(data[['Age']])
+plt.xlabel('Age (years)')
+plt.ylabel('count')
+plt.show()
