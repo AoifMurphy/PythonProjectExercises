f4fe7e19a332cb5b76e74ae617b62c4b0d28547c
AoifMurphy <79363725+AoifMurphy@users.noreply.github.com>
2021-04-04 00:28:28 +0100

    April 4 updated

---
+++ Exercises9.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.palplot(sns.color_palette("Purples", 8))
+plt.show()
+
+sns.palplot(sns.color_palette("husl", 10))
+plt.show()
+
+sns.palplot(sns.color_palette("coolwarm", 6))
+plt.show()
